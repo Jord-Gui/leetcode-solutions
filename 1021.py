cffc0e7b7cfbeabfc28c685096f5beb08a4aec44
Jord <jgui0003@student.monash.edu>
2020-06-01 21:00:03 +1000

    first solution

--- 1021.py
+++ 1021.py
@@ -2,16 +2,23 @@
 
 def removeOuterParentheses(S: str) -> str:
     primitives = []
-    stack = []
+    parity = 0
     current_primitive = ""
     for bracket in S:
+        current_primitive += bracket
         if bracket == "(":
-            stack.append(bracket)
-        elif bracket == ")":
-            current_primitive = stack.pop() + current_primitive + bracket
-        if len(stack) == 0:
+            parity += 1
+        else:
+            parity -= 1
+        if parity == 0:
             primitives.append(current_primitive)
             current_primitive = ""
-    return primitives
+    for i in range(len(primitives)):
+        primitives[i] = primitives[i][1:len(primitives[i])-1]
+    ret_val = ""
+    for brackets in primitives:
+        ret_val += brackets
+    return ret_val
 
 print(removeOuterParentheses("(()())(())"))
+print(removeOuterParentheses("(()())(())(()(()))"))
