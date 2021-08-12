d894f438ec8d62525ae4ecad6df9a18262cdefc1
Jord Gui <jord_gui@hotmail.com>
2021-08-12 19:50:08 +1000

    Add solution for 7

--- 7.py
+++ 7.py
@@ -2,16 +2,19 @@
 
 class Solution:
     def reverse(self, x: int) -> int:
-        if x == 0 or x > 2**31 - 1 or x < -2**31:
-            return 0
-        elif x < 0:
-            x = x*-1
-            ret_val = "-"
+        str_x = str(x)
+        reverse_x = ""
+        if x > 0:
+            for num in range(len(str_x)):
+                reverse_x = str_x[num] + reverse_x
+        elif x == 0:
+            reverse_x = 0
         else:
-            ret_val = ""
-        while x != 0:
-            ret_val += str(x % 10)
-            if int(ret_val) > 2**31 - 1 or int(ret_val) < -2**31:
-                return 0
-            x = x // 10
-        return int(ret_val)
+            for num in range(1, len(str_x)):
+                reverse_x = str_x[num] + reverse_x
+            reverse_x = "-" + reverse_x
+        reverse_x = int(reverse_x)
+        if reverse_x > (2**31 - 1) or reverse_x < (-2**31):
+            return 0
+        else:
+            return reverse_x
